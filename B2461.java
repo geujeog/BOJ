a7454e1fb3132e1fb510b9f7dca05c0a34838615
kang jihyeong <her4386@gmail.com>
2023-05-13 23:31:34 +0900

    백준 2461번

--- B2461.java
+++ B2461.java
@@ -10,17 +10,17 @@ public class B2461 {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            List<Tuple> queue = new ArrayList<>();
+            List<Tuple> list = new ArrayList<>();
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
 
                 for (int j = 0; j < M; j++) {
-                    queue.add(new Tuple(i, Integer.parseInt(st.nextToken())));
+                    list.add(new Tuple(i, Integer.parseInt(st.nextToken())));
                 }
             }
 
-            Collections.sort(queue, new Comparator<Tuple>(){
+            Collections.sort(list, new Comparator<Tuple>(){
                 @Override
                 public int compare (Tuple t1, Tuple t2) {
                     return Integer.compare(t1.number, t2.number);
@@ -34,17 +34,17 @@ public class B2461 {
             Map<Integer, Integer> map = new HashMap<>();
 
             while (end < N*M) {
-                Tuple e = queue.get(end);
+                Tuple e = list.get(end);
 
                 map.put(e.idx, map.getOrDefault(e.idx, 0)+1);
-                while (map.get(queue.get(start).idx) > 1) {
-                    map.put(queue.get(start).idx, map.get(queue.get(start).idx)-1);
+                while (map.get(list.get(start).idx) > 1) {
+                    map.put(list.get(start).idx, map.get(list.get(start).idx)-1);
 
-                    if (map.get(queue.get(start).idx) == 0) map.remove(queue.get(start).idx);
+                    if (map.get(list.get(start).idx) == 0) map.remove(list.get(start).idx);
                     start++;
                 }
 
-                if (map.size() == N) result = Math.min(result, e.number - queue.get(start).number);
+                if (map.size() == N) result = Math.min(result, e.number - list.get(start).number);
 
                 end++;
             }
