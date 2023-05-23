6e3251f9cc63bf394e17325ccba8b741e880ccdb
kang jihyeong <her4386@gmail.com>
2023-05-23 20:17:04 +0900

    백준 1967번

---
+++ B1967.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+public class B1967 {
+    static int result;
+    static int[][] dp;
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<Tuple>[] list = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<Tuple>();
+        }
+
+        boolean[] isParent = new boolean[N+1];
+
+        for (int i = 1; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            list[v1].add(new Tuple(v2, value));
+
+            isParent[v1] = true;
+        }
+
+        // 0행 : 자식노드 중 제일 큰 값
+        // 1행 : 자식노드 중 제일 큰 2개 합치기
+        dp = new int[N+1][2];
+
+        getLen(list, 1);
+
+        bw.write(result+"");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void getLen (List<Tuple>[] list, int v) {
+        int maxFirst = 0;
+        int maxSecond = 0;
+
+        for (Tuple tuple : list[v]) {
+            int i = tuple.child;
+            int value = tuple.value;
+
+            getLen(list, i);
+
+            if (maxFirst < dp[i][0] + value) {
+                maxSecond = maxFirst;
+                maxFirst = dp[i][0] + value;
+            }
+            else {
+                maxSecond = Math.max(maxSecond, dp[i][0] + value);
+            }
+        }
+        dp[v][0] = maxFirst;
+        dp[v][1] = maxFirst + maxSecond;
+
+        result = Math.max(result, Math.max(dp[v][0], dp[v][1]));
+    }
+
+    public static class Tuple {
+        int child;
+        int value;
+
+        public Tuple (int child, int value) {
+            this.child = child;
+            this.value = value;
+        }
+    }
+}
