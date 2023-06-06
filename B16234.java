6bb2c871383dc1ed7ca36a8ee8ef6072ba368e18
kang jihyeong <her4386@gmail.com>
2023-06-06 16:48:11 +0900

    백준 16234번 속도 개선

--- B16234.java
+++ B16234.java
@@ -35,8 +35,7 @@ public class B16234 {
     public static int bfs () {
         int dateCnt = 0;
 
-        while (true) {
-            if (!isOpen()) break;
+        while (isOpen()) {
             dateCnt++;
         }
 
@@ -46,39 +45,41 @@ public class B16234 {
     public static boolean isOpen () {
         boolean open = false;
 
-        List<List<Tuple>> country = new ArrayList<>();
+        int[][] sum = new int[2][N*N]; // [0]행에는 같은 연합 인구수, [1]행에는 같은 연합 나라수
+        int[] parent = new int[N*N]; // 같은 연합
+        initParent(parent);
+
         boolean[][] check = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!check[i][j]) {
-                    checkCountry(i, j, check, country);
+                    checkCountry(i, j, check, parent);
                 }
             }
         }
 
-        // 연합이 있을 경우
-        if (country.size() != N*N) {
-            for (List<Tuple> countryGroup : country) {
-                int countryCnt = countryGroup.size();
-                int populationCnt = countryGroup.stream().mapToInt(o -> o.population).sum();
-                for (Tuple cg : countryGroup) {
-                    int calculate = (int) (populationCnt / countryCnt);
+        // 인구수, 나라수 카운팅
+        for (int i = 0; i < N*N; i++) {
+            int p = find(parent, i);
+            sum[0][p] += arr[i/N][i%N];
+            sum[1][p]++;
+        }
 
-                    if (arr[cg.x][cg.y] != calculate) open = true;
+        // 같은 연합끼리 인구수 나눔
+        for (int i = 0; i < N*N; i++) {
+            int p = find(parent, i);
 
-                    arr[cg.x][cg.y] = calculate;
-                }
-            }
+            int calculate = (int) (sum[0][p] / sum[1][p]);
+
+            if (calculate != arr[i/N][i%N]) open = true;
+            arr[i/N][i%N] = calculate;
         }
 
         return open;
     }
 
-    public static void checkCountry (int i, int j, boolean[][] check, List<List<Tuple>> country) {
-        List<Tuple> countryGroup = new ArrayList<>();
-        countryGroup.add(new Tuple(i, j, arr[i][j]));
-
+    public static void checkCountry (int i, int j, boolean[][] check, int[] parent) {
         Queue<Tuple> queue = new LinkedList<>();
         queue.add(new Tuple(i, j, 0));
 
@@ -95,7 +96,7 @@ public class B16234 {
                 int diff = Math.abs(beforePopulation - arr[x][y]);
 
                 if (diff >= L && diff <= R) {
-                    countryGroup.add(new Tuple(x, y, arr[x][y]));
+                    union(parent, i*N+j, x*N+y);
                 }
                 else continue;
             }
@@ -107,8 +108,6 @@ public class B16234 {
             queue.add(new Tuple(x, y-1, arr[x][y]));
             queue.add(new Tuple(x, y+1, arr[x][y]));
         }
-
-        country.add(countryGroup);
     }
 
     public static class Tuple {
@@ -120,6 +119,30 @@ public class B16234 {
             this.x = x;
             this.y = y;
             this.population = population;
+        }
+    }
+
+    public static void initParent (int[] parent) {
+        for (int i = 0; i < N*N; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public static int find (int[] parent, int x) {
+        if (parent[x] == x) return x;
+
+        return find(parent, parent[x]);
+    }
+
+    public static void union (int[] parent, int x, int y) {
+        x = find(parent, x);
+        y = find(parent, y);
+
+        if (x < y) {
+            parent[y] = x;
+        }
+        else {
+            parent[x] = y;
         }
     }
 }
