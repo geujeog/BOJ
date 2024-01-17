7925a6e580d72270b965e580eed4ef575b3f6fa4
kang jihyeong <her4386@gmail.com>
2024-01-17 19:00:48 +0900

    백준 2098번

--- B2098.java
+++ B2098.java
@@ -3,44 +3,41 @@ import java.io.*;
 
 class B2098 {
     static int N;
-    static int[][] arr;
-    static int[][] dp;
-    static int INF = 987654321;
+    static int[][] weight;
+    static Integer[][] dp;
     static int result;
 
     public static void main (String[] args) throws IOException {
-        init();
+        input();
         solution();
-        print();
+        output();
     }
 
     public static void solution() {
-        result = tsp(0, 1);
+        result = dfs(0, 1);
     }
 
-    public static int tsp(int city, int bit) {
-        if (bit == (1 << N) - 1) {
-            if (arr[city][0] == 0) return INF;
-            else return arr[city][0];
+    public static int dfs(int v, int visit) {
+        if (visit == (1 << N) - 1) {
+            if (weight[v][0] == 0) return Integer.MAX_VALUE;
+            return weight[v][0];
         }
 
-        if (dp[city][bit] != -1) return dp[city][bit];
+        if (dp[v][visit] == null) {
+            dp[v][visit] = Integer.MAX_VALUE;
 
-        dp[city][bit] = INF;
+            for (int i = 0; i < N; i++) {
+                int next = visit | (1 << i);
 
-        for (int i = 0; i < N; i++) {
-            // 경로가 없거나 이미 방문했을 경우
-            if (arr[city][i] == 0 || (bit & (1 << i)) != 0) continue;
-
-            int next = bit | (1 << i);
-
-            dp[city][bit] = Math.min(dp[city][bit], tsp(i, next) + arr[city][i]);
+                if (weight[v][i] == 0 || (visit & (1 << i)) != 0) continue;
+                dp[v][visit] = Math.min(dp[v][visit], dfs(i, next) + weight[v][i]);
+            }
         }
 
-        return dp[city][bit];
+        return dp[v][visit];
     }
 
-    public static void print() throws IOException {
+    public static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         bw.write(result+"");
@@ -49,22 +46,20 @@ class B2098 {
         bw.close();
     }
 
-    public static void init() throws IOException {
+    public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        dp = new int[N][(1 << N) - 1];
-
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        weight = new int[N][N];
+        dp = new Integer[N][(1 << N) - 1];
+        result = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                int w = Integer.parseInt(st.nextToken());
+                weight[i][j] = w;
             }
         }
 
