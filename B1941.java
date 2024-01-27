de1fb34a05cf22009144f98ef5c18b3c0de73598
kang jihyeong <her4386@gmail.com>
2024-01-27 09:35:05 +0900

    백준 1941번

--- B1941.java
+++ B1941.java
@@ -2,90 +2,117 @@ import java.util.*;
 import java.io.*;
 
 class B1941 {
-    static char[] seat;
-    static int[] index;
-    static int result = 0;
+    static int N;
+    static char[][] arr;
+    static char[] arrLine;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static int result;
 
     public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        seat = new char[25];
-        for (int i = 0; i < 5; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < 5; j++) {
-                seat[5*i+j] = line.charAt(j);
-            }
-        }
-
-        index = new int[7];
-        // 25C7
-        getIndex(0, 0, 0, 0);
-
-        bw.write(result+"");
-
-        br.close();
-        bw.flush();
-        bw.close();
+        input();
+        solution();
+        output();
     }
 
-    public static void getIndex (int idx, int count, int yCount, int sCount) {
-        if (idx == 25 || count == 7) {
-            if (count == 7 && sCount > 3) {
-                if (aroundBFS()) result++;
+    public static void solution() {
+        for (int i = 0; i < N*N; i++) {
+            int sCnt = (arrLine[i] == 'S') ? 1 : 0;
+            int yCnt = (arrLine[i] == 'Y') ? 1 : 0;
+
+            boolean[][] contains = new boolean[N][N];
+
+            contains[i / N][i % N] = true;
+            choice(i, sCnt, yCnt, contains);
+            contains[i / N][i % N] = false;
+        }
+    }
+
+    public static void choice(int idx, int sCnt, int yCnt, boolean[][] contains) {
+        if (yCnt > 3) return;
+        if (sCnt + yCnt == 7) {
+            if (isPossible(contains)) {
+                result++;
             }
             return;
         }
 
-        index[count] = idx;
+        for (int i = idx+1; i < N*N; i++) {
+            int sp = (arrLine[i] == 'S') ? 1 : 0;
+            int yp = (arrLine[i] == 'Y') ? 1 : 0;
 
-        if (seat[idx] == 'Y') getIndex(idx+1, count+1, yCount+1, sCount);
-        else getIndex(idx+1, count+1, yCount, sCount+1);
-
-        getIndex(idx+1, count, yCount, sCount);
+            contains[i / N][i % N] = true;
+            choice(i, sCnt + sp, yCnt + yp, contains);
+            contains[i / N][i % N] = false;
+        }
     }
 
-    public static boolean aroundBFS () {
-        boolean[][] check = new boolean[5][5];
-        boolean[][] go = new boolean[5][5];
-        for (int i = 0; i < 7; i++) {
-            // System.out.print(index[i]+" ");
-            go[index[i]/5][index[i]%5] = true;
-        }
-        // System.out.println();
+    public static boolean isPossible(boolean[][] contains) {
+        int[] start = getStart(contains);
 
-        Queue<Tuple> queue = new LinkedList<>();
-        queue.add(new Tuple(index[0]%5, index[0]/5));
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visit = new boolean[N][N];
+        int cnt = 1;
 
-        int cnt = 0;
+        visit[start[0]][start[1]] = true;
+        queue.add(start);
 
         while (!queue.isEmpty()) {
-            Tuple tuple = queue.poll();
-            int x = tuple.x;
-            int y = tuple.y;
+            int[] q = queue.poll();
 
-            if (x < 0 || y < 0 || x == 5 || y == 5) continue;
-            if (!go[y][x] || check[y][x]) continue;
+            for (int d = 0; d < dx.length; d++) {
+                int nx = q[0] + dx[d];
+                int ny = q[1] + dy[d];
 
-            check[y][x] = true;
-            cnt++;
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if (!contains[nx][ny] || visit[nx][ny]) continue;
 
-            queue.add(new Tuple(x-1, y));
-            queue.add(new Tuple(x+1, y));
-            queue.add(new Tuple(x, y-1));
-            queue.add(new Tuple(x, y+1));
+                cnt++;
+                visit[nx][ny] = true;
+                queue.add(new int[]{nx, ny});
+            }
         }
+
         if (cnt == 7) return true;
         return false;
     }
 
-    public static class Tuple {
-        int x;
-        int y;
-
-        public Tuple (int x, int y) {
-            this.x = x;
-            this.y = y;
+    public static int[] getStart(boolean[][] contains) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (contains[i][j]) {
+                    return new int[] {i, j};
+                }
+            }
         }
+
+        return new int[2];
+    }
+
+    public static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(result+"");
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = 5;
+        arr = new char[N][N];
+        arrLine = new char[N*N];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = line.charAt(j);
+                arrLine[i * N + j] = arr[i][j];
+            }
+        }
+
+        br.close();
     }
 }
